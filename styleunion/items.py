51fbfc7cfdc91a2031afd6e79e99d36ba6d18887
sreedevi-ps <valliyyathu@gmail.com>
2025-11-14 23:33:14 +0530

    added full working spider with pagination and json paarsing with all fields

--- styleunion/items.py
+++ styleunion/items.py
@@ -7,6 +7,17 @@ import scrapy
 
 
 class StyleunionItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+   
+  product_url= scrapy.Field()
+  product_name= scrapy.Field()
+  price= scrapy.Field()
+  sku= scrapy.Field()
+  size= scrapy.Field()
+  color= scrapy.Field()
+  size_list= scrapy.Field()
+  color_list= scrapy.Field()
+  description= scrapy.Field()
+  care_instructions= scrapy.Field()
+  image_urls= scrapy.Field()
+  product_details= scrapy.Field()
+  currency= scrapy.Field()
