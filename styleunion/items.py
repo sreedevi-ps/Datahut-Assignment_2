f483e17562f05ab44682f7af1d54321b5340e4c3
sreedevi-ps <valliyyathu@gmail.com>
2025-11-15 15:30:29 +0000

    restarted from scratch

---
+++ styleunion/items.py
@@ -0,0 +1,20 @@
+from scrapy import Item, Field
+
+
+class ProductItem(Item):
+    product_url = Field()
+    product_name = Field()
+    price = Field()
+    sku = Field()
+    size = Field()  
+    color = Field()  
+    size_list = Field()
+    color_list = Field()
+    description = Field()
+    care_instructions = Field()
+    image_urls = Field()
+    product_details = Field()  
+    currency = Field()
+
+    # <-- Newline
+
